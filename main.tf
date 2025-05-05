ded3af16b0ef584fc7430ec8cc5e81417c043582
Shaik Shahul <shahulsonu123@gmail.com>
2025-05-05 11:32:19 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+region = "ap-south-1"
+}
+
+resource "aws_instance" "one" {
+count = 4
+ami = "ami-04aa00acb1165b32a"
+instance_type = "t2.medium"
+key_name = "keypair"
+vpc_security_group_ids = ["sg-06785bc60f40dceda"]
+tags = {
+Name = var.instance_names[count.index]
+}
+}
+
+variable "instance_names" {
+default = ["jenkins", "tomcat-1", "tomcat-2", "Monitoring server"]
+}
