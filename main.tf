429d0502439838c09977a9e62a744c11d87ebf23
Shaik Shahul <shahulsonu123@gmail.com>
2025-05-06 11:12:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ region = "ap-south-1"
 
 resource "aws_instance" "one" {
 count = 4
-ami = "ami-04aa00acb1165b32a"
-instance_type = "t2.medium"
-key_name = "keypair"
-vpc_security_group_ids = ["sg-06785bc60f40dceda"]
+ami = "ami-03edbe403ec8522ed"
+instance_type = "t2.micro"
+key_name = "awskeypair"
+vpc_security_group_ids = ["sg-041530ece07ff5134"]
 tags = {
 Name = var.instance_names[count.index]
 }
